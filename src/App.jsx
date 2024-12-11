26ac7264d56cea6af89b0a61272a72484f351ba1
dbryant <d.bryant718@gmail.com>
2024-12-11 15:38:38 -0500

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,53 @@
+// src/App.jsx
+
+import './App.css';
+import 'src/components/IngredientList.jsx'
+import 'src/components/BurgerStack.jsx'
+import IngredientList from './components/IngredientList';
+import { useState } from 'react';
+
+export const availableIngredients = [
+  { name: 'Kaiser Bun', color: 'saddlebrown' },
+  { name: 'Sesame Bun', color: 'sandybrown' },
+  { name: 'Gluten Free Bun', color: 'peru' },
+  { name: 'Lettuce Wrap', color: 'olivedrab' },
+  { name: 'Beef Patty', color: '#3F250B' },
+  { name: 'Soy Patty', color: '#3F250B' },
+  { name: 'Black Bean Patty', color: '#3F250B' },
+  { name: 'Chicken Patty', color: 'burlywood' },
+  { name: 'Lettuce', color: 'lawngreen' },
+  { name: 'Tomato', color: 'tomato' },
+  { name: 'Bacon', color: 'maroon' },
+  { name: 'Onion', color: 'lightyellow' },
+  { name: 'Cheddar Cheese', color: '#FDE18B' },
+  { name: 'Swiss Cheese', color: '#F1E1A8' },
+];
+
+const App = () => {
+  const [stack, setStack] = useState();
+
+
+  const addToBurger = (props) => {
+    props.preventDefault();
+  }
+
+  const removeFromBurger = () => {
+
+  }
+
+
+  return (
+    <>
+    
+    <main>
+      <h1>Burger Stacker</h1>
+      <section>
+        <IngredientList />
+        <BurgerStack />
+      </section>
+    </main>
+    </>
+  );
+};
+
+export default App;
